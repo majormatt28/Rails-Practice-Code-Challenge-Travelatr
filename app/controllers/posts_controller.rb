3cdce98cca5975b71354a66cc97f25a1ac9006f7
Matthew <elijah2895@hotmail.com>
2021-03-22 16:22:11 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
+        #byebug
     end
 
     def show
@@ -33,6 +34,6 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:title, :content)
+        params.require(:post).permit(:title, :content, :likes)
     end
 end
